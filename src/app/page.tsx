d4a3b4037569b164cbc14b6424de1da695bb73d6
hasini <hahahasini0210@gmail.com>
2025-09-24 16:47:25 +0000

    but y did u change the footer colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import Contact from '@/components/sections/contact';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-primary">
+    <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-grow">
         <Hero />
