afbe8c752284603ca6df06e1af41393d8b293e26
hasini <hahahasini0210@gmail.com>
2025-09-24 16:53:06 +0000

    hey i want following fonts in my website Playfair Display: This is a cla

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Lora:wght@400;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Playfair+Display:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
         <script
